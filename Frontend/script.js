ad4f7dc8c73a6d39a468eea278d948991f8b6d67
Thanh Nguyen Khac <thanhcqb2048@gmail.com>
2023-12-26 21:47:52 +0700

    responsive image and video

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,12 +1,19 @@
-window.addEventListener('load', updateImageSize);
+window.addEventListener('load', updateImageSizeForSingleMedia);
 
-function updateImageSize() {
-    // Lấy ra phần tử có id là mediaContainer
-    var mediaContainer = document.getElementById('mediaContainer');
-    // Lấy ra phần tử con đầu tiên là img
-    var img = mediaContainer.querySelector('img');
-    if (img.clientWidth > mediaContainer.clientWidth) {
-        img.style.width = '100%';
-        img.style.height = 'auto';
+function updateImageSizeForSingleMedia() {
+    var singleMediaNodes = document.querySelectorAll('.threads__post__body__singlemedia');
+    console.log(singleMediaNodes.length);
+    for (var i = 0; i < singleMediaNodes.length; i++) {
+        var img = singleMediaNodes[i].querySelector('img');
+        if(img == null){
+            img = singleMediaNodes[i].querySelector('video');
+        }
+        img.style.width = 'auto';
+        img.style.height = '60vh';
+        if (img.clientWidth > singleMediaNodes[i].clientWidth) {
+            img.style.width = '100%';
+            img.style.height = 'auto';
+        } 
     }
+
 }
